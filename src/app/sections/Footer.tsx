5a40fef01d7d345a4768ca452f85d9bfd6b63090
Aurelia Lim <aureliadotlim@gmail.com>
2023-06-27 13:57:52 +0800

    add footer

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -1,50 +1,50 @@
-// import Link from "next/link";
-// import React, { useEffect, useState } from "react";
-// import { FaRegStar } from "react-icons/fa";
-// import { FiGitBranch } from "react-icons/fi";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { FaRegStar } from "react-icons/fa";
+import { FiGitBranch } from "react-icons/fi";
 
-// function Footer() {
-//   const [githubInfo, setGitHubInfo] = useState({
-//     stars: null,
-//     forks: null,
-//   });
+function Footer() {
+  const [githubInfo, setGitHubInfo] = useState({
+    stars: null,
+    forks: null,
+  });
 
-//   useEffect(() => {
-//     fetch("")
-//       .then((response) => response.json())
-//       .then((json) => {
-//         const { stargazers_count, forks_count } = json;
-//         setGitHubInfo({
-//           stars: stargazers_count,
-//           forks: forks_count,
-//         });
-//       })
-//       .catch((e) => console.error(e));
-//   }, []);
+  useEffect(() => {
+    fetch("")
+      .then((response) => response.json())
+      .then((json) => {
+        const { stargazers_count, forks_count } = json;
+        setGitHubInfo({
+          stars: stargazers_count,
+          forks: forks_count,
+        });
+      })
+      .catch((e) => console.error(e));
+  }, []);
 
-//   return (
-//     <footer>
-//       <Link
-//         href=""
-//         target="_blank"
-//         className="footer-link"
-//       >
-//         <span className="footer-info">Designed and Built by Aurelia Lim.</span>
-//         {githubInfo && (
-//           <div className="footer-git">
-//             <div className="footer-git-item">
-//               <FaRegStar className="footer-git-item-icon" />
-//               <span className="footer-git-item-text">{githubInfo.stars}</span>
-//             </div>
-//             <div className="footer-git-item">
-//               <FiGitBranch className="footer-git-item-icon" />
-//               <span className="footer-git-item-text">{githubInfo.forks}</span>
-//             </div>
-//           </div>
-//         )}
-//       </Link>
-//     </footer>
-//   );
-// }
+  return (
+    <footer>
+      <Link
+        href=""
+        target="_blank"
+        className="footer-link"
+      >
+        <span className="footer-info">Designed and Built by Aurelia Lim.</span>
+        {githubInfo && (
+          <div className="footer-git">
+            <div className="footer-git-item">
+              <FaRegStar className="footer-git-item-icon" />
+              <span className="footer-git-item-text">{githubInfo.stars}</span>
+            </div>
+            <div className="footer-git-item">
+              <FiGitBranch className="footer-git-item-icon" />
+              <span className="footer-git-item-text">{githubInfo.forks}</span>
+            </div>
+          </div>
+        )}
+      </Link>
+    </footer>
+  );
+}
 
-// export default Footer;
+export default Footer;
