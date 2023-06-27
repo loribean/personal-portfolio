d94f3639e2f028f44f643098c7decbfa49877b75
Aurelia Lim <aureliadotlim@gmail.com>
2023-06-27 13:51:40 +0800

    copy changes

--- src/app/sections/Contact.tsx
+++ src/app/sections/Contact.tsx
@@ -18,9 +18,7 @@ function Contact() {
       <h2 className="contact-title">What&apos;s Next?</h2>
       <h2 className="contact-sub-title">Get In Touch</h2>
       <p className="contact-text">
-        Although I’m not currently looking for any new opportunities, my inbox
-        is always open. Whether you have a question or just want to say hi, I’ll
-        try my best to get back to you!
+        Drop me an email, I'll get back to you in a jiffy.
       </p>
       <div className="contact-cta">
         <Button link="mailto:aureliadotlim@gmail.com" text="Say Hello" />
