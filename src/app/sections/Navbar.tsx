f8c8dcd5ac7fa3641be71942b06348765605b038
Aurelia Lim <aureliadotlim@gmail.com>
2023-06-27 14:43:41 +0800

    update CV

--- src/app/sections/Navbar.tsx
+++ src/app/sections/Navbar.tsx
@@ -123,7 +123,7 @@ function Navbar() {
               delay: 0.6,
             }}
           >
-            <Button text="Resume" link="http://localhost:3000/resume.pdf" />
+            <Button text="Resume" link="/CV.pdf" />
           </motion.div>
         </div>
       </div>
